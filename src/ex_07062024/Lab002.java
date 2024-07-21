75af2aee5e99ee6c773b45e82c688104ddd2b965
OnkarTurankar <onkar.turankar34@gmail.com>
2024-07-21 17:46:13 +0530

    All data

---
+++ src/ex_07062024/Lab002.java
@@ -0,0 +1,15 @@
+package ex_07062024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("2+3");
+//        System.out.println(2+3);
+        System.out.println(3/4);
+        System.out.println(3/4);
+        System.out.println(3/4);
+        System.out.println(3/4);
+        System.out.println(3/4);
+        System.out.println(3/4);
+        System.out.println(3/4);
+    }
+}
