c22a25765da3392b05db7681ec8a1a37b90f0062
OnkarTurankar <onkar.turankar34@gmail.com>
2024-07-27 12:02:46 +0530

    27-07-24

---
+++ src/ex_07062024/Lab009.java
@@ -0,0 +1,12 @@
+package ex_07062024;
+
+public class Lab009 {
+    public static void main(String[] args) {
+//        replacement %S is for string and %D is for integer, Use printf instead of pritnln
+
+        String label= "Rahul";
+        String name= "Chahul";
+        System.out.printf("Your_Name_is -> %S",label);
+        System.out.printf("Your_Name_is -> %S",name);
+    }
+}
---
+++ src/ex_07062024/Lab010.java
@@ -0,0 +1,9 @@
+package ex_07062024;
+
+public class Lab010 {
+    public static void main(String[] args) {
+        int age= 34;
+        System.out.println(age);
+//        here int=data type, age=variable name, 34=value or literal
+    }
+}
