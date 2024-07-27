33d42345d8d55845f2442fce156b5379dc68f1fc
OnkarTurankar <onkar.turankar34@gmail.com>
2024-07-27 12:36:30 +0530

    27-07-24

---
+++ src/ex_07062024/Lab012.java
@@ -0,0 +1,10 @@
+package ex_07062024;
+
+public class Lab012 {
+    public static void main(String[] args) {
+        int age=30;
+        double age=35;
+
+//        variable should be unique
+    }
+}
