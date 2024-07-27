066dd81422200f26274e444f996a5b98f217e9d7
OnkarTurankar <onkar.turankar34@gmail.com>
2024-07-27 23:07:49 +0530

    27-07-24

--- src/ex_07062024/Lab012.java
+++ src/ex_07062024/Lab012.java
@@ -3,7 +3,7 @@ package ex_07062024;
 public class Lab012 {
     public static void main(String[] args) {
         int age=30;
-        double age=35;
+
 
 //        variable should be unique
     }
---
+++ src/ex_07062024/Lab014.java
@@ -0,0 +1,11 @@
+package ex_07062024;
+
+public class Lab014 {
+    public static void main(String[] args) {
+//        constant =final
+//        final is a keyword given to not to change the value in the program
+       final int a=30;
+        int b=23;
+        System.out.println(a*b);
+    }
+}
