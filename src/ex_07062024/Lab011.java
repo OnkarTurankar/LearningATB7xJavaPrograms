5a52d343c6846ab4af757079ec4ae6336f6caeb9
OnkarTurankar <onkar.turankar34@gmail.com>
2024-07-27 12:15:17 +0530

    27-07-24

---
+++ src/ex_07062024/Lab011.java
@@ -0,0 +1,26 @@
+package ex_07062024;
+
+public class Lab011 {
+    public static void main(String[] args) {
+//        calculator
+        double a=34;
+        double b=10;
+        double sum=a+b;
+        double div=a/b;
+        double mul=a*b;
+        double sub=a-b;
+        System.out.printf("sum is %f",sum);
+        System.out.println();
+        System.out.printf("div is %f",div);
+        System.out.println();
+        System.out.printf("mul is %f",mul);
+        System.out.println();
+        System.out.printf("sub is %f",sub);
+        System.out.println();
+//        or
+        System.out.printf("sum is %f\n",sum);
+        System.out.printf("div is %f\n",div);
+        System.out.printf("mul is %f\n",mul);
+        System.out.printf("sub is %f\n",sub);
+    }
+}
